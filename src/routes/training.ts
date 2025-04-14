07e40b5c0e941722313a886e103e04cf7f4588c1
Anastasia Nikulkina <nikoulkina@gmail.com>
2025-04-14 16:33:12 -0700

    Added endpoints for creating, modifying, deleting questions in admin dashboard.

--- src/routes/training.ts
+++ src/routes/training.ts
@@ -1,11 +1,16 @@
-import express from 'express';
-import { getQuestions, submitAnswer, getSubmissions } from '../controllers/training';
-import auth from '../middleware/authentication';
+import express, {NextFunction} from "express";
+import { getQuestions, submitAnswer, getSubmissions, createQuestion, updateQuestion, deleteQuestion } from "../controllers/training";
+import auth, {adminOnly} from "../middleware/authentication";
 
 const router = express.Router();
+
+
 
 router.get('/:topic/:type', auth, getQuestions);
 router.post('/:topic/:type/submit', auth, submitAnswer);
 router.get('/:topic/:type/submissions', auth, getSubmissions);
+router.post('/questions', auth, adminOnly, createQuestion); // Create a new question
+router.patch('/questions/:id', auth, adminOnly, updateQuestion); // Update an existing question
+router.delete('/questions/:id', auth, adminOnly, deleteQuestion);
 
 export default router;
