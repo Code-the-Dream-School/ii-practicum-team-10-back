a6644b91ea1813ba638b9604636774fcae14a540
Anastasia Nikulkina <47100243+nasnik@users.noreply.github.com>
2025-04-28 17:32:35 -0700

    adding and updating JavaScript challenges to database feature (#8)
    * changed Question model to exclude answers of mandatory fields.
    
    * changed the adding question validation object
    
    * changed the adding question validation object for flashcards
    
    * changed the adding question validation object for quiz
    
    * changed the adding question validation object for challenges
    
    * updated updateQuestion controller and swagger docs

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -1,8 +1,8 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface ITest {
-    input: string;
-    expectedOutput: string;
+    input: any[];
+    expectedOutput: any;
 }
 
 export interface IQuestion extends Document {
@@ -10,8 +10,8 @@ export interface IQuestion extends Document {
     type: string;
     codeSnippet?: string;
     questionText: string;
-    answers: string[];
-    questionSuggestedAnswers?: string[]; 
+    answers?: string[];
+    questionSuggestedAnswers?: string[];
     tests?: ITest[];
 }
 
@@ -36,7 +36,7 @@ const QuestionSchema = new Schema<IQuestion>({
     },
     answers: {
         type: [String],
-        required: [true, 'Please provide at least one answer'],
+        required: false,
     },
     questionSuggestedAnswers: {
         type: [String],
@@ -56,8 +56,8 @@ const QuestionSchema = new Schema<IQuestion>({
     tests: {
         type: [
             {
-                input: { type: String, required: true },
-                expectedOutput: { type: String, required: true },
+                input: { type: [Schema.Types.Mixed], required: true },
+                expectedOutput: { type: Schema.Types.Mixed, required: true },
             },
         ],
         required: [
